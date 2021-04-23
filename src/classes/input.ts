00d5b882e845920d5ea6602f030f1fc0d7800a45
ssamsonov <ssamsonosv@waviot.ru>
2021-04-23 10:28:40 +0300

    Added different inputs class

--- src/classes/input.ts
+++ src/classes/input.ts
@@ -1,10 +1,78 @@
-const {
-    Input,
-    Select,
-    Confirm,
-    Survey
-} = require('enquirer');
-
 namespace Aik {
-    export class Input {}
+    export class Input {
+        /** Get simple line from user */
+        public static Line(question: string) : string {
+            let done = false;
+            let data = null;
+
+            // Prepare
+            let {Input} = require('enquirer');
+            let input = new Input({
+                message: question
+            });
+
+            // Run
+            try {
+                input.run().then(result => {
+                    data = result;
+                    done = true;
+                });
+                require('deasync').loopWhile(() => !done);
+            } catch (e) {
+                return '';
+            }
+            return data;
+        }
+
+        /** Get choice of list from user */
+        public static Choice(question: string, variants: string[]) : string {
+            let done = false;
+            let data = null;
+
+            // Prepare
+            let {Select} = require('enquirer');
+            let input = new Select({
+                message: question,
+                choices: variants
+            });
+
+            // Run
+            try {
+                input.run().then(result => {
+                    data = result;
+                    done = true;
+                });
+                require('deasync').loopWhile(() => !done);
+            } catch (e) {
+                return '';
+            }
+            return data;
+        }
+
+        /** Get boolean from user */
+        public static Toggle(question: string, trueVariant: string, falseVariant: string) : string {
+            let done = false;
+            let data = null;
+
+            // Prepare
+            let {Toggle} = require('enquirer');
+            let input = new Toggle({
+                message: question,
+                enabled: trueVariant,
+                disabled: falseVariant
+            });
+
+            // Run
+            try {
+                input.run().then(result => {
+                    data = result;
+                    done = true;
+                });
+                require('deasync').loopWhile(() => !done);
+            } catch (e) {
+                return '';
+            }
+            return data;
+        }
+    }
 }
