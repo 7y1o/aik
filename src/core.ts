1afb2ffe5aeb436588415f0edc3d0c4c2b38f54a
ssamsonov <ssamsonosv@waviot.ru>
2021-04-23 13:51:37 +0300

    Added run, check input and process line functions. Changed export method

--- src/core.ts
+++ src/core.ts
@@ -1,19 +1,62 @@
 const ansi = require('ansi-colors');
 
 namespace Aik {
-    class Core {
+    export class Core {
         private data: Aik.Data;
 
         /** Initialize assistant */
         constructor() {
             this.data = new Aik.Data();
 
-            // Check if
+            // Check if user runs assistant program first time
             if(this.data.FirstRun) {
-                //
+                Aik.Setup.RunSetup();
             }
+
+            // After checks, run assistant
+            this.RunAssistant();
+        }
+
+        /** Run main loop */
+        public RunAssistant() {
+            let doneInput: boolean = false;
+            let inputResult: string = '';
+
+            while (true) {
+                doneInput = false;
+                inputResult = '';
+
+                // Prepare input
+                let {Input} = require('enquirer');
+                let getInput = new Input({
+                    message: 'Вы'
+                });
+
+                // Run input
+                getInput.run().then(result => {
+                    inputResult = result;
+                    doneInput = true;
+                });
+                require('deasync').loopWhile(() => !doneInput);
+
+                // If user input is empty, pass
+                if(!inputResult) continue;
+
+                // Else process it
+                this.CheckInput(inputResult);
+            }
+        }
+
+        /** Check input */
+        private CheckInput(line: string) : string {
+            // TODO: Get input, tokenize it and find needed mindem for execute
+            return '';
+        }
+
+        /** Replace variables */
+        private ProcessLine(line: string) : string {
+            // TODO: Get string line and replace variables in it
+            return '';
         }
     }
 }
-
-module.exports = Aik;
