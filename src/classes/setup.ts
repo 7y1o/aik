a4ab9dfb0df67fb45e74786b6ad5a374150623f3
ssamsonov <ssamsonosv@waviot.ru>
2021-04-23 03:26:55 +0300

    Created setup script

---
+++ src/classes/setup.ts
@@ -0,0 +1,10 @@
+namespace Aik {
+    export class Setup {
+        /** Run setup */
+        public static run() {
+            let templates: any = Aik.Data.LoadJSON(`${__dirname}/../../data/templates.json`);
+
+
+        }
+    }
+}
