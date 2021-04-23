4b2d6f0318069ae45f5728970c256a2d841cb706
ssamsonov <ssamsonosv@waviot.ru>
2021-04-23 18:50:20 +0300

    Created base class for mindems
    Signed-off-by: ssamsonov <ssamsonosv@waviot.ru>

---
+++ src/mindems/_base.ts
@@ -0,0 +1,11 @@
+interface MindemStore {
+    name: string,
+    value: string | number
+}
+
+export default class Mindem {
+    private local: MindemStore[];
+
+    /** Constructor */
+    constructor() {}
+}
