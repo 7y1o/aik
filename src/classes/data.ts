8ade38531d7f0c5704712eeaf97d5797942ed939
ssamsonov <ssamsonosv@waviot.ru>
2021-04-23 17:02:02 +0300

    Added save and read variable

--- src/classes/data.ts
+++ src/classes/data.ts
@@ -27,6 +27,21 @@ namespace Aik {
             return dataHolder;
         }
 
+        /** Save variable */
+        saveVariable(variable: string, value: any, force: boolean = false) {
+            if(this.saved[variable] !== undefined && !force) {
+                return;
+            }
+
+            this.saved[variable] = value;
+            fs.writeFileSync(`${__dirname}/../../data/saved.json`, JSON.stringify(this.saved, null, 2));
+        }
+
+        /** Get variable */
+        readVariable(variable: string) : any {
+            return this.saved[variable] || '';
+        }
+
         /** Check if user runs assistant first time */
         public get FirstRun(): boolean {
             return !this.saved.name || !this.saved.search;
