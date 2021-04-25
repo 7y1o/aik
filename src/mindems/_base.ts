8cf5a7734fbb8ebcc2f044bb10c94d787464193b
7y1o <tdesu.webdesign@gmail.com>
2021-04-25 04:41:14 +0000

    Added ProcessVariables function for search and replace variables. Added Data function for work with local variables

--- src/mindems/_base.ts
+++ src/mindems/_base.ts
@@ -1,11 +1,50 @@
+const path = require('path');
+
 interface MindemStore {
     name: string,
-    value: string | number
+    value: any
 }
 
 export default class Mindem {
     private local: MindemStore[];
+    private templates: string[];
 
     /** Constructor */
-    constructor() {}
+    public constructor(name: string) {
+        this.templates = require(path.resolve(name));
+        
+    }
+
+    /** Function called before run */
+    public PreLaunch(words: string[]) : boolean {
+        return true;
+    }
+
+    /** Function called after preLaunch */
+    public Launch(words: string[]) : any[] | null {
+        return null;
+    }
+    
+    /** Callback function (called after launch) */
+    public PostLaunch(words: string[], response: string, postData: any[]) : boolean {
+        return true;
+    }
+
+    /** Read output string and replace variables in it */
+    public ProcessVariables(output: string) : string {
+        Object.keys(this.local).forEach(variable => {
+            output = output.replaceAll(`$${variable}`, this.local[variable]);
+        });
+        return output;
+    }
+
+    /** Work with local data */
+    protected Data(key: string, value: any = null) : any {
+        if(!value) {
+            return this.local[key] || null;
+        } else {
+            this.local[key] = value;
+            return true;
+        }
+    }
 }
